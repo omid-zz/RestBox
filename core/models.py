6796b144228a33ddd1ffaf5ce4eaa5c96cfdb647
omidzz <mr.omid.zamzami@gmail.com>
2026-07-04 10:30:34 +0330

    added custom user model and authtoken app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    roles = [('host', 'host'),('guest', 'guest')]
+    full_name = models.CharField(max_length= 80)
+    email = models.EmailField(unique= True)
+    role = models.CharField(max_length= 8, choices= roles)
+    create_time = models.DateTimeField(auto_now_add= True)
+    username_field = 'email'
+    required_fields = ['username', 'full_name', 'role']
+    def __str__(self):
+        return f'{self.full_name} {self.role}'
--- restbox_project/settings.py
+++ restbox_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'core',
 ]
 
@@ -117,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'core.User'
