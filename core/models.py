36db0a7f3b1a24cc66187f9c3beadceaf03dfc75
Hamid <hrj161615@gmail.com>
2026-07-06 16:49:36 +0330

    Add Villa and Reservation models

--- core/models.py
+++ core/models.py
@@ -11,3 +11,21 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['username', 'full_name', 'role']
     def __str__(self):
         return f'{self.full_name} {self.role}'
+
+class Villa(models.Model):
+    host = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    address = models.TextField(max_length=100)
+    price_per_night = models.IntegerField()
+    capacity = models.PositiveIntegerField()
+    amenities = models.JSONField()
+
+class Reservation(models.Model):
+    guest_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    villa_id = models.ForeignKey(Villa, on_delete=models.CASCADE)
+    check_in = models.DateField()
+    check_out = models.DateField()
+    total_price = models.IntegerField()
+    status = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
